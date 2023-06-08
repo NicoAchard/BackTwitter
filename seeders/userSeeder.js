e0f0215a4d16b75ce70eeef6bc8fc7f3f1be6bf0
ffierro4 <facundofierro49@gmail.com>
2023-06-08 19:16:53 -0300

    Update userSeeder.js

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -1,17 +1,17 @@
 /**
- * El seeder no es más que un archivo que contiene una función que se encarga
- * de insertar datos (generalmente de prueba) en una base de datos.
- *
- * El nombre "seeder" es una convención y significa "semillero".
- *
- * Además, en este caso, se está usando una librería llamada Faker
- * (https://fakerjs.dev/) para facilitar la creación de datos ficticios como
- * nombres, apellidos, títulos, direcciones y demás textos.
- *
- * Suele ser común que en los seeders exista un `for` donde se define la
- * cantidad de registros de prueba que se insertarán en la base de datos.
- *
- */
+ 
+El seeder no es más que un archivo que contiene una función que se encarga
+de insertar datos (generalmente de prueba) en una base de datos.
+*
+El nombre "seeder" es una convención y significa "semillero".
+*
+Además, en este caso, se está usando una librería llamada Faker
+(https://fakerjs.dev/) para facilitar la creación de datos ficticios como
+nombres, apellidos, títulos, direcciones y demás textos.
+*
+Suele ser común que en los seeders exista un for donde se define la
+cantidad de registros de prueba que se insertarán en la base de datos.
+**/
 
 const { faker } = require("@faker-js/faker");
 const User = require("../models/User");
@@ -21,19 +21,34 @@ faker.locale = "es";
 
 module.exports = async () => {
   const users = [];
+  const images = [
+    "/img/random1.png",
+    "/img/random2.png",
+    "/img/random3.png",
+    "/img/random4.png",
+    "/img/random5.png",
+    "/img/random6.png",
+    "/img/random7.png",
+    "/img/random8.png",
+    "/img/random9.png",
+    "/img/random10.png",
+  ];
 
   for (let i = 0; i < 100; i++) {
     let email = faker.internet.email();
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+
     users.push({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
+      firstname: faker.name.firstName(),
+      lastname: faker.name.lastName(),
       username: email,
       email: email,
       password: await bcrypt.hash("123", 2),
       createdAt: faker.date.past(),
       updatedAt: faker.date.recent(),
       description: faker.lorem.sentence(2),
-      profilePicture: "/img/default_avatar.png",
+      profilePicture: randomImage,
       tweetList: [],
       following: [],
       followers: [],
