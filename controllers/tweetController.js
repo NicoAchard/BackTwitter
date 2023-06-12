6e84f65318f6fb2ae7b45ff64d0202a77321d34f
ffierro4 <facundofierro49@gmail.com>
2023-06-12 12:35:00 -0300

    delete function

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -49,12 +49,12 @@ async function update(req, res) {}
 // Remove the specified resource from storage.
 async function destroy(req, res) {
   const user = await User.findById(req.auth.id).populate("tweetList");
-  await Tweet.findByIdAndDelete(req.params.id);
+  const deletedTweet = await Tweet.findByIdAndDelete(req.params.id);
 
   user.tweetList = user.tweetList.filter((tweet) => tweet.id.toString() !== req.params.id);
   await user.save();
 
-  return res.json({ tweetList: user.tweetList, user });
+  return res.json({ tweetId: deletedTweet._id });
 }
 
 async function like(req, res) {
